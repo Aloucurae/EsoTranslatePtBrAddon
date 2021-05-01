1ff17a78fc2b7fd4e6de2c56f96a52efa6d77094
Alex Jonas <alexj.desantanna@gmaol.com>
2021-05-01 16:57:23 -0300

    iniciando trabalhos

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 const exoExtrac = require('./lib/EsoExtractData');
 const log = require('./lib/log/log');
+const mongo = require('./lib/db/mongo');
+const utils = require('./lib/utils/utils');
 
-const basegameFolder = 'F:\\games\\TESO\\The Elder Scrolls Online';
+async function start() {
 
-let extract = false;
+    const basegameFolder = 'F:\\games\\TESO\\The Elder Scrolls Online';
 
+    let extract = false;
 
-if (extract) {
-    log.log("Atualizando arquvios base");
+    if (extract) {
+        log.log("Atualizando arquvios base");
 
-    exoExtrac(basegameFolder);
+        exoExtrac(basegameFolder);
+    }
+
+    log.log("Verificando atualizacoes");
+
+    // verifica se tem novas traducoes
+    utils.getNotTranslate();
+
+    // verificar ja traduzidas
+    // e traduzilas talvez usando o rabbit
+    // gerar arquvios .lua para o addon    
+    
+    
+    // TODO
+    // criar arquivo do docker para traducao solo
 }
+
+start();
+// log.log("Verificando atualizacoes");
 
 
